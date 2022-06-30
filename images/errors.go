bc1780747e656e733a073e85b61bb25c9b030922
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-06-30 15:14:25 +0100

    Add a comment to the exported variable in images/errors.go

--- images/errors.go
+++ images/errors.go
@@ -2,6 +2,7 @@ package images
 
 import "errors"
 
+// ImageErrors contains a list of errors that can be encountered by functionaltiy in the images package
 var ImageErrors = map[string]error{
 	"400": errors.New("400: Bad Request"),
 }
